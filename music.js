700146297636e262c6e7094a46f62e7058e54267
Lina198 <54925800+Lina198@users.noreply.github.com>
2020-04-06 09:57:48 +0700

    htrj

--- music.js
+++ music.js
@@ -1,17 +1,3 @@
-var btn = document.querySelector("button");
-var canv = document.getElementById('canvas');
-var ctx = canv.getContext('2d');
-canv.width = window.innerWidth;
-canv.height = window.innerHeight;
-var count=0;
-function once(){
-    alert("ПРИВЕТ Я ЗАШЕЛ СЮДА");
-    if(count==0) {ctx.strokeRect(50, 50, 100, 100);count++} else
-    if(count==1) {ctx.strokeRect(200, 50, 100, 100);count++} else
-    if(count==2) {ctx.strokeRect(350, 50, 100, 100);count=0}
-}
-btn.addEventListener('click', once);
-
 var app = angular.module('myApp', []);
 app.controller('customersCtrl', function($scope,$http) {
     $http.get("music.json").then(function(response) {
@@ -53,4 +39,18 @@ function tableSearch() {
         }
     }
 }*/
+var btn = document.querySelector("button");
+var canv = document.getElementById('canvas');
+var ctx = canv.getContext('2d');
+canv.width = window.innerWidth;
+canv.height = window.innerHeight;
+var count=0;
+function once(){
+    alert("ПРИВЕТ Я ЗАШЕЛ СЮДА");
+    if(count==0) {ctx.strokeRect(50, 50, 100, 100);count++} else
+    if(count==1) {ctx.strokeRect(200, 50, 100, 100);count++} else
+    if(count==2) {ctx.strokeRect(350, 50, 100, 100);count=0}
+}
+btn.addEventListener('click', once);
+
 
