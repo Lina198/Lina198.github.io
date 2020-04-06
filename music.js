f75ce582ac45feb94c1465be5584622f400a23da
Lina198 <54925800+Lina198@users.noreply.github.com>
2020-04-06 14:46:53 +0700

    пшлрщ

--- music.js
+++ music.js
@@ -24,33 +24,25 @@ function tableSearch() {
 
     }
 }
-/*function Rectangle() {
-    var canv = document.getElementById('canvas');
-    var ctx = canv.getContext('2d');
-    canv.width = window.innerWidth;
-    canv.height = window.innerHeight;
-    var x=0;
-    if (canv.getContext) {
-    for(i=0;i<5;i++) {
-            x=x+150;
-        ctx.fillText("Имя группы", x, 60);
-            ctx.moveTo(x, 50);
-        ctx.strokeRect(x, 50, 100, 100);
-        }
-    }
-}*/
-var btn = document.querySelector("button");
+
+var btn = document.querySelector('.morti');
 var canv = document.getElementById('canvas');
 var ctx = canv.getContext('2d');
 canv.width = window.innerWidth;
 canv.height = window.innerHeight;
 var count=0;
 function once(){
-    alert("ПРИВЕТ Я ЗАШЕЛ СЮДА");
+    if(count==10){alert("БОЛЬШЕ УВЫ НЕЛЬЗЯ")}
     if(count==0) {ctx.strokeRect(50, 50, 100, 100);count++} else
     if(count==1) {ctx.strokeRect(200, 50, 100, 100);count++} else
-    if(count==2) {ctx.strokeRect(350, 50, 100, 100);count=0}
+    if(count==2) {ctx.strokeRect(350, 50, 100, 100);count++} else
+    if(count==3) {ctx.strokeRect(500, 50, 100, 100);count++} else
+    if(count==4) {ctx.strokeRect(650, 50, 100, 100);count++} else
+    if(count==5) {ctx.strokeRect(800, 50, 100, 100);count++} else
+    if(count==6) {ctx.strokeRect(950, 50, 100, 100);count++} else
+    if(count==7) {ctx.strokeRect(1100, 50, 100, 100);count++} else
+    if(count==8) {ctx.strokeRect(1250, 50, 100, 100);count++} else
+    if(count==9) {ctx.strokeRect(1400, 50, 100, 100);count++}
 }
-btn.addEventListener('click', once);
 
 
